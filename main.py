8f84abcc3203714adcea78c7a7ecbfd808c051eb
thhonigm <thonigmann@web.de>
2025-01-18 02:35:05 +0100

    Ch2 L5

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
     while True:
@@ -11,6 +13,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color = (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
